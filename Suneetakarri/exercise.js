2dcede326d529aeb917af9591f470c5b840a44df
Suneeta Karri <suneetak@mit.edu>
2017-03-02 19:57:12 -0500

    hw boston
    hw boston

--- Suneetakarri/exercise.js
+++ Suneetakarri/exercise.js
@@ -4,18 +4,13 @@ var exercise = {};
 
 Boston City Data - Start of our Data Science journey
 In your code do not use any "for loops" - use Array Callback style
-
 */
-
 //exercise.maxEarnings = function() {
-
     /* EX 1
-
     replace the for loop with one of the array callback functions
     first create an array of total earnings with overtime
 
     */
-
 //     var currentMax = 0.0;
 //     var people = exercise.data.data;
 //     var dataLength = people.length;
@@ -38,6 +33,12 @@ In your code do not use any "for loops" - use Array Callback style
 var getEarnings = function(people){
     return Number(people[18]);
 };
+var getBaseEarnings = function(people){
+    return Number(people[11]);
+};
+var getZipCodes = function(zip){
+    return Number(zip[19]);
+};
 exercise.maxEarnings = function() {
     var people = exercise.data.data;
     // function getEarnings(people){
@@ -56,60 +57,56 @@ exercise.maxEarnings = function() {
     return cmax;
  //   return earnings;
 };
-
-
-
 exercise.earningsAbove = function(target) {
-
     /* EX 2
-
     return the number of people with salaries above "target"
     use filter to create an array of salaries above "target"
     then return the length of the array
-
     */
     var people = exercise.data.data;
     var numSalary = people.map(getEarnings); // get handle on data
-    
     var isAboveTargetSal = function(item){
         var sal = (item>target);
         return sal;
         }
-
     var myarray = numSalary.filter(isAboveTargetSal);
     return myarray.length;
 };
 
 exercise.totalBasePayroll = function() {
-
+    var people = exercise.data.data;
+    var numSalary = people.map(getBaseEarnings);
+    var baseSal = numSalary.reduce((a,b) => a+b,0);
+    return Math.floor(baseSal);
     /* EX 3
-
     return the total payroll for Boston City as an integer
     use Map and Reduce to do this
-
     */
-
 };
 
 exercise.totalEarningsWithOvertime = function() {
-
+    var people = exercise.data.data;
+    var numSalary = people.map(getEarnings);
+    var baseSal = numSalary.reduce((a,b) => a+b,0);
+    return Math.floor(baseSal);
     /* EX 4
-
     return the total Earnings with Overtime as an integer
-
     */
-
 };
 
 exercise.numberUniqueZipCodes = function() {
-
-    /* EX 5
-
-    return the unique number zipcodes as an integer
-
-    */
-
+    var zip = exercise.data.data;
+    var zCod = zip.map(getZipCodes);
+    // var countZip = function(item){
     var zipCodes = [];
-
+    for (var i = 0; i < zCod.length; i++) {
+        if (!(zipCodes.includes(zCod[i]))) {
+            zipCodes.push(zCod[i]);
+            }
+    }
     return zipCodes.length;
-};
+    //}
+    /* EX 5
+    return the unique number of zipcodes as an integer
+    */
+ };
